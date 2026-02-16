86e351d8f6f01a16e49963f2356d8ec9054f5f50
Ronaldo Hoch <ronaldopintohoch@gmail.com>
2026-02-16 19:50:24 -0300

    feat: Otimização de imagens

--- site/src/app/core/footer/footer.ts
+++ site/src/app/core/footer/footer.ts
@@ -1,7 +1,9 @@
+import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
+  imports:[NgOptimizedImage],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl:'./footer.html'
 })
