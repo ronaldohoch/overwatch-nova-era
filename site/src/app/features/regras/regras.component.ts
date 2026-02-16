684e3c4a8deea11a9c78a0c9072cd8fc2850e869
Ronaldo Hoch <ronaldopintohoch@gmail.com>
2026-02-16 19:33:23 -0300

    WIP: Adiciona início de regras

---
+++ site/src/app/features/regras/regras.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-regras',
+  imports: [],
+  templateUrl: './regras.component.html',
+  styleUrl: './regras.component.css',
+})
+export class RegrasComponent {
+
+}
