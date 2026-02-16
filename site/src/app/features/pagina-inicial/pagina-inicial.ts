9c2887e3b115e6f8c3d68968e173bf80dbc47c03
Ronaldo Hoch <ronaldopintohoch@gmail.com>
2026-02-16 16:20:37 -0300

    wip: Adiciona o component de pagina inicial

--- site/src/app/features/pagina-inicial/pagina-inicial.ts
+++ site/src/app/features/pagina-inicial/pagina-inicial.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { ButtonsComponent } from '../../shared/buttons/buttons';
+// import { ButtonsComponent } from '../../shared/buttons/buttons';
+import { HeroComponent } from './components/hero/hero.component';
 
 @Component({
   selector: 'app-pagina-inicial',
-  imports: [ButtonsComponent],
+  imports: [HeroComponent],
   templateUrl: './pagina-inicial.html',
   styleUrl: './pagina-inicial.css',
 })
