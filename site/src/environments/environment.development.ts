727a7913c33ff26d0fce28afb084f7e14391d73f
Ronaldo Hoch <ronaldopintohoch@gmail.com>
2026-02-16 22:18:12 -0300

    feat: Adiciona os environments

---
+++ site/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiURL:'http://127.0.0.1:5001/copa-nova-era-overwatch/us-central1',
+  apiURLAuth: 'http://127.0.0.1:5001/copa-nova-era-overwatch/us-central1/auth'
+};
---
+++ site/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiURL: 'https://us-central1-copa-nova-era-overwatch.cloudfunctions.net',
+  apiURLAuth: 'https://us-central1-copa-nova-era-overwatch.cloudfunctions.net/auth',
+};
